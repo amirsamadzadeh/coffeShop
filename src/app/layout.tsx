6fc5b46c5189bb08cf50d11a104d1d0a9be31b95
amirsamadzadeh <amirhsmz@yahoo.com>
2026-06-01 14:50:46 +0330

    feat: setup font in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const vazirmatn = Vazirmatn({
+  subsets: ["arabic"],
+  weight: ["400", "500", "700"],
+});
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
@@ -24,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={vazirmatn.className}>{children}</body>
     </html>
   );
 }
