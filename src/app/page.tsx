26268c9b479a3aa8da4cf5134d6864471086de4b
amirsamadzadeh <amirhsmz@yahoo.com>
2026-06-01 15:10:11 +0330

    feat: add simple navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import { Navbar } from "@/components/modules/Navbar";
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <Navbar/>
     </div>
   );
 }
