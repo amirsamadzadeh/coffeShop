cc0a35a416a62ed6bb8e323a76b458f14d78611b
amirsamadzadeh <amirhsmz@yahoo.com>
2026-06-08 16:41:38 +0330

    feat(categories): add category creation endpoint

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,39 @@
+import connectDB from "@/configs/db";
+import Category from "@/models/Categorie";
+
+export async function POST(req: Request) {
+  try {
+    await connectDB();
+    const body = await req.json();
+    const { name, slug } = body;
+
+    if (name.trim() === "" || slug.trim() === "") {
+      return Response.json(
+        { message: "name and slug is required" },
+        { status: 400 },
+      );
+    }
+
+    const isCategoryExist = await Category.findOne({
+      $or: [{ slug }, { name }],
+    });
+
+    if (isCategoryExist) {
+      return Response.json(
+        { message: "Category is already exist." },
+        { status: 409 },
+      );
+    }
+
+    await Category.create({ name, slug });
+
+    return Response.json(
+      { message: "Category created Successfully" },
+      { status: 201 },
+    );
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return Response.json({ message: err.message }, { status: 500 });
+    }
+  }
+}
