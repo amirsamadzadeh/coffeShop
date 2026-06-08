48da52915cbfcd06491926e458153d8a6e12718f
amirsamadzadeh <amirhsmz@yahoo.com>
2026-06-08 16:27:51 +0330

    feat : adding slug field to Categorie Schema Model

--- src/models/Categorie.ts
+++ src/models/Categorie.ts
@@ -4,6 +4,11 @@ const CategorySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
 
 const Category =
